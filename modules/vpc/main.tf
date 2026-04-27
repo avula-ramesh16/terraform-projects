9d154971a3234688c0b20968fd25cbb759cd888f
avula-ramesh16 <avula.ramesh169@gmail.com>
2026-04-27 13:20:35 +0530

    Envs Approval

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -27,6 +27,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
 
     tags= {
@@ -62,6 +63,7 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1a"
 
     tags = {
       Name = "Private_Subnet"
